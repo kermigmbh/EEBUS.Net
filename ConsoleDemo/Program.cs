059f8a159c25dbdcf95525d4002f29eb39c8b5e4
kerschal <kerschl.alexander@kermi.de>
2026-01-27 11:48:44 +0100

    add demo

---
+++ ConsoleDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using EEBUS;
+using EEBUS.Net;
+using Microsoft.Extensions.Options;
+
+namespace ConsoleDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MDNSClient mDNSClient = new MDNSClient();
+            MDNSService mDNSService = new MDNSService(Options.Create<Settings>(new Settings()
+            {
+                Device = new DeviceSettings()
+                {
+                    Name = "ConsoleDemoDevice",
+                    Id = "1",
+                    Model = "DemoModel",
+                     Serial = "123456",
+                      Port = 7200
+                     
+                },
+
+            }));
+            var manager = new EEBUSManager(mDNSClient, mDNSService);
+        }
+    }
+}
