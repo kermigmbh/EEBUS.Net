b0757fc6403bfc379656801a0aeeae4d2cc70d9f
Euler Urs <Euler.Urs@kermi.de>
2026-02-16 13:14:49 +0100

    add remote ski to event

--- EEBUS.Net/EEBUS/UseCases/ControllableSystem/Events.cs
+++ EEBUS.Net/EEBUS/UseCases/ControllableSystem/Events.cs
@@ -4,22 +4,22 @@ namespace EEBUS.UseCases.ControllableSystem
 {
 	public interface LPCEvents : UseCaseEvents
 	{
-		Task DataUpdateLimitAsync( int counter, bool active, long limit, TimeSpan duration );
+		Task DataUpdateLimitAsync( int counter, bool active, long limit, TimeSpan duration, string remoteSki);
 
-        Task DataUpdateFailsafeConsumptionActivePowerLimitAsync( int counter, long limit );
+        Task DataUpdateFailsafeConsumptionActivePowerLimitAsync( int counter, long limit, string remoteSki);
 	}
 
 	public interface LPPEvents : UseCaseEvents
 	{
-        Task DataUpdateLimitAsync( int counter, bool active, long limit, TimeSpan duration );
+        Task DataUpdateLimitAsync( int counter, bool active, long limit, TimeSpan duration, string remoteSki);
 
-        Task DataUpdateFailsafeProductionActivePowerLimitAsync( int counter, long limit );
+        Task DataUpdateFailsafeProductionActivePowerLimitAsync( int counter, long limit, string remoteSki);
 	}
 
 	public interface LPCorLPPEvents : UseCaseEvents
 	{
-        Task DataUpdateFailsafeDurationMinimumAsync( int counter, TimeSpan duration );
+        Task DataUpdateFailsafeDurationMinimumAsync( int counter, TimeSpan duration, string remoteSki);
 
-        Task DataUpdateHeartbeatAsync( int counter, RemoteDevice device, uint timeout );
+        Task DataUpdateHeartbeatAsync( int counter, RemoteDevice device, uint timeout, string remoteSki);
 	}
 }
