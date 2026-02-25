6015e5fc6a35db51b79953f352ded1fc3d5356dc
Euler Urs <Euler.Urs@agmm.net>
2026-02-25 09:54:20 +0100

    add test class

---
+++ TestProject1/SpineMessageTests.cs
@@ -0,0 +1,38 @@
+﻿using EEBUS;
+using EEBUS.Messages;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace TestProject1
+{
+    public class SpineMessageTests
+    {
+        public SpineMessageTests()
+        {
+            foreach (string ns in new string[] {"EEBUS.SHIP.Messages", "EEBUS.SPINE.Commands", "EEBUS.Entities",
+                                                 "EEBUS.UseCases.ControllableSystem", "EEBUS.UseCases.GridConnectionPoint",
+                                                 "EEBUS.Features" })
+            {
+                foreach (Type type in GetTypesInNamespace(typeof(Settings).Assembly, ns))
+                    RuntimeHelpers.RunClassConstructor(type.TypeHandle);
+            }
+        }
+
+        private static Type[] GetTypesInNamespace(Assembly assembly, string nameSpace)
+        {
+            return assembly.GetTypes()
+                            .Where(t => String.Equals(t.Namespace, nameSpace, StringComparison.Ordinal))
+                            .ToArray();
+        }
+
+        [Fact]
+        public Task SendMessageTestAsync()
+        {
+            string messageToSend = EEBusMessages.LoadControl_Write_DeleteTimePeriod_AndUpdate;
+            return Task.CompletedTask;
+        }
+    }
+}
