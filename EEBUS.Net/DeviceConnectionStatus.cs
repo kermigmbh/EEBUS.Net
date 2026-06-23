59eeaeb88ec36cb5d597ce3fc8006c85d08d5d2f
Alexander Kerschl <kerschl.alexander@kermi.de>
2026-06-23 10:46:25 +0200

    add option

--- EEBUS.Net/DeviceConnectionStatus.cs
+++ EEBUS.Net/DeviceConnectionStatus.cs
@@ -6,6 +6,9 @@ namespace EEBUS.Net
 {
     public enum DeviceConnectionStatus
     {
+        /// <summary>
+        /// Unknown - not connected
+        /// </summary>
         Unknown = 0,
         UseCaseDiscoveryCompleted = 1,
         NodeDiscoveryCompleted = 2,
